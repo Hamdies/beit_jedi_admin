3314dd78e82dfee6c92fbff9f1cc9c86c9884116
Ahmed Hamdy <ahmedhamdy5882178@gmail.com>
2026-05-14 16:12:20 +0300

    fix: remove versioning from order view CSS link for consistency

--- resources/views/vendor-views/order/order-view.blade.php
+++ resources/views/vendor-views/order/order-view.blade.php
@@ -21,8 +21,7 @@ active
 @section('title', 'تفاصيل الطلب #' . $order['id'])
 
 @push('css_or_js')
-@php($orderViewCssVersion = file_exists(public_path('assets/admin/css/order-view.css')) ? filemtime(public_path('assets/admin/css/order-view.css')) : time())
-<link rel="stylesheet" href="{{ dynamicAsset('public/assets/admin/css/order-view.css') }}?v={{ $orderViewCssVersion }}">
+<link rel="stylesheet" href="{{ dynamicAsset('public/assets/admin/css/order-view.css') }}">
 @endpush
 
 @section('content')
